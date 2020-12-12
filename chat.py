13a03f42b7b267e4144735b357a899f06361bb96
FEDJA <fedor.n585@gmail.com>
2020-12-12 18:58:20 +0300

    first chat.py file commit

---
+++ chat.py
@@ -0,0 +1,5 @@
+def send_message(sender, recipient, message):
+    print(f'User: {sender} texted for {recipient}:')
+    print(message)
+
+send_message('Fedos', 'Feodor', "Hi! It's my first message :)")
