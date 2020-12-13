2a0798c3ca09d936317e807c65a37e84ef2619d1
Feodor <feodor.n@gmail.com>
2020-12-13 14:01:03 +0300

    Второй коммит. Это я в своей ветке

--- chat.py
+++ chat.py
@@ -4,3 +4,4 @@ def send_message(sender, recipient, message):
 
 send_message('Fedos', 'Feodor', "Hi! It's my first message :)")
 send_message('Feodor', 'Fedos', "Hi! How are you?")
+# Теперь я буду работать в своей ветке
